6b62c82dcbe45ca70e69f13d4c225d5495de2458
Nicholas <nicholasjoven@gmail.com>
2022-08-24 11:20:41 -0400

    added rack cors and news feed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,41 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        let req = await fetch('http://localhost:3100/posts')
+        if (req.ok) {
+          let res = await req.json()
+          setPosts(res)
+        } else {
+          alert('Posts could not be loaded.')
+        }
+      } catch(error) {
+        alert(error.message)
+      }
+    }
+    getPosts()
+  }, [])
+
   return (
     <div className="App">
-      wow
+      <h2>News Feed</h2>
+      {posts.map(post => {
+        const { id, title, content, likes_count, user_id } = post
+        return (
+          <div key={id}>
+            <h4>{title}</h4>
+            <p>{content}</p>
+          </div>
+        )
+      })}
+      <div>
+        <button>New Post</button>
+      </div>
     </div>
   );
 }
