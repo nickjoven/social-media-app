eefdc92e8c3f29f8f33cb41189b6dae1ed54fcab
Nicholas <nicholasjoven@gmail.com>
2022-08-24 11:59:22 -0400

    created form

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ## This is documentation. Perhaps.
 
 The default port for the Rails server has been set to port 3100 to avoid conflicts with `npm start`.
+
+## TODO
+- [ ] Log in as a user (redux)
+- [ ] View individual posts
+- [ ] Add images to videos/posts
+
--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './App.css';
 
 const App = () => {
   const [posts, setPosts] = useState([])
+  const form = useRef()
 
   useEffect(() => {
     const getPosts = async () => {
@@ -21,8 +22,34 @@ const App = () => {
     getPosts()
   }, [])
 
+  const handleAddPost = () => {
+
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const data = new FormData(form.current)
+    let req = await fetch('http://localhost:3100/login', {
+      method: 'POST',
+      body: data
+    })
+    if (req.ok) {
+      alert('You have logged in')
+    } else {
+      alert('Invalid login info')
+    }
+  }
+
   return (
     <div className="App">
+      <h2>Log in</h2>
+      <form onSubmit={handleSubmit} ref={form}>
+        <input type='email' placeholder='email' name='email' /><br />
+        <input type='password' placeholder='password' name='password' /><br />
+        <input type='submit' />
+        <br />
+      </form>
+      <hr />
       <h2>News Feed</h2>
       {posts.map(post => {
         const { id, title, content, likes_count, user_id } = post
